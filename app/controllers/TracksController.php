270e4dfb03aec88ecb2b76a2816cb52673044fff
Jason <jbibbings@cenmi.org>
2015-07-13 10:13:01 -0400

    Deleted old files started a new test.

--- app/controllers/TracksController.php
+++ app/controllers/TracksController.php
@@ -29,9 +29,9 @@ class TracksController extends BaseController {
 	$node = array('id' => 22, array('prev' = 2, 'next' => 'results', 'path' => '2.1.2'));
 	$node = array('id' => 23, array('prev' = 5, 'next' => 'results', 'path' => '1.1.1.2.1'));
 	$node = array('id' => 24, array('prev' = 5, 'next' => 'results', 'path' => '1.1.1.2.2'));
-	$node = array('id' => 25, array('prev' = 5, 'next' => '7', 'path' => '1.1.2.1.1.1'));
-	$node = array('id' => 26, array('prev' = 5, 'next' => 'results', 'path' => '1.1.2.1.1.1'));
-	$node = array('id' => 27, array('prev' = 5, 'next' => 'results', 'path' => '1.1.2.1.1.2'));
+	$node = array('id' => 25, array('prev' = 5, 'next' => 7, 'path' => '1.1.2.1.1.1'));
+	$node = array('id' => 26, array('prev' = 5, 'next' => 'results', 'path' => '1.1.2.1.1.2'));
+	$node = array('id' => 27, array('prev' = 5, 'next' => 7, 'path' => '1.1.2.1.1.2'));
 	$node = array('id' => 28, array('prev' = 6, 'next' => 'results', 'path' => '1.1.2.1.1.1.1'));
 	$node = array('id' => 29, array('prev' = 6, 'next' => 'results', 'path' => '1.1.2.1.1.1.2'));
 	$node = array('id' => 30, array('prev' = 6, 'next' => 'results', 'path' => '1.1.2.1.2.1.1'));
@@ -39,112 +39,6 @@ class TracksController extends BaseController {
 
 
 
-
-
-
-
-
-
-
-
-		/*
-		private $tracks; 
-		$tracks[0] = array(
-			'1' => 'yes', 
-			'2' => 'yes', 
-			'3' => 'yes', 
-			'4' => 'yes', 
-			'5' => 'yes'
-		);
-		$tracks[1] = array(
-			'1' => 'no',
-			'2' => 'no'
-		);
-		$tracks[2] = array(
-			'1' => 'yes', 
-			'2' => 'no', 
-			'3' => 'yes'
-		);
-		$tracks[3] = array(
-			'1' => 'yes',
-			'2' => 'no',
-			'3' => 'no',
-			'4' => 'no'
-		);
-		$tracks[4] = array(
-			'1' => 'yes',
-			'2' => 'yes',
-			'3' => 'no',
-			'4' => 'no',
-			'5' => 'yes'
-		);
-		$tracks[5] = array(
-			'1' => 'no',
-			'2' => 'yes',
-			'5' => 'yes'
-		);
-		$tracks[6] = array(
-			'1' => 'no',
-			'2' => 'yes',
-			'5' => 'no'
-		);
-		$tracks[7] = array(
-			'1' => 'yes',
-			'2' => 'yes',
-			'3' => 'yes',
-			'5' => 'no',
-			'6' => 'yes'
-		);
-		$tracks[8] = array(
-			'1' => 'yes',
-			'2' => 'yes',
-			'3' => 'yes',
-			'5' => 'no',
-			'6' => 'no'
-		);
-		$tracks[9] = array(
-			'1' => 'yes',
-			'2' => 'no',
-			'3' => 'no',
-			'4' => 'yes',
-			'6' => 'no'
-		);
-		$tracks[10] = array(
-			'1' => 'yes',
-			'2' => 'yes',
-			'3' => 'no',
-			'4' => 'yes',
-			'5' => 'yes',
-			'6' => 'yes',
-			'7' => 'yes'
-		);
-		$tracks[11] = array(
-			'1' => 'yes',
-			'2' => 'yes',
-			'3' => 'no',
-			'4' => 'yes',
-			'5' => 'yes',
-			'6' => 'yes',
-			'7' => 'no'
-		);
-		$tracks[12] = array(
-			'1' => 'yes',
-			'2' => 'yes',
-			'3' => 'no',
-			'4' => 'yes',
-			'5' => 'no',
-			'6' => 'yes',
-			'7' => 'yes'
-		);
-		$tracks[13] = array(
-			'1' => 'yes',
-			'2' => 'yes',
-			'3' => 'no',
-			'4' => 'yes',
-			'5' => 'yes',
-			'6' => 'yes',
-			'7' => 'no'
-		);*/
 
 	
 	/** 
---
+++ app/tests/SurveyTest.php
@@ -0,0 +1,17 @@
+<?php
+
+class SurveyTest extends TestCase {
+
+	/**
+	 * A basic functional test example.
+	 *
+	 * @return void
+	 */
+	public function verifies_paths_are_correct()
+	{
+		$path = '1';
+
+		$this->assertTrue($this->client->getResponse()->isOk());
+	}
+
+}
