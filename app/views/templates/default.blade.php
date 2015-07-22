b3af6e5e8d4b85e5d10c71d673a79363affcf4d3
Jason <jbibbings@cenmi.org>
2015-07-22 09:25:16 -0400

    Added responsive stylesheet framework

--- app/views/templates/default.blade.php
+++ app/views/templates/default.blade.php
@@ -1,19 +1,47 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<title>Decision Making Tree</title>
+
+  <!-- Basic Page Needs
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <meta charset="utf-8">
+  <title>Vroom-Yetton-Jago Decision Model</title>
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <!-- Mobile Specific Metas
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- FONT
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link href="//fonts.googleapis.com/css?family=Raleway:400,300,600" rel="stylesheet" type="text/css">
+
+  <!-- CSS
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+	{{ HTML::style('css/normalize.css') }}
+	{{ HTML::style('css/skeleton.css') }}
+
+  <!-- Favicon
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link rel="icon" type="image/png" href="images/favicon.png">
+
 </head>
 <body>
-	<header>
-		<h1>Vroom-Yetton-Jago Decision Model</h1>
-	</header>
-	
-	<div id="main">
-		@yield('content')
-	</div>
 
+  <!-- Primary Page Layout
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <header><h1>Vroom-Yetton-Jago Decision Model</h1></header>
 
-	<footer></footer>
+  <div class="container">
+    <div class="row">
+      <div class="one-half column" style="margin-top: 25%">
+        @yield('content')
+    </div>
+  </div>
+
+  <footer></footer>
+<!-- End Document
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
 </body>
 </html>
